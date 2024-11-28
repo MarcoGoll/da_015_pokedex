dde27b045f7713eb64351674a7dba6824e463028
MarcoGoll <166357601+MarcoGoll@users.noreply.github.com>
2024-11-28 20:53:43 +0100

    create: logic for overviewpage

--- script.js
+++ script.js
@@ -1,14 +1,151 @@
 /*====================================================================================================
     GLOBAL VARIABLES
 ====================================================================================================*/ 
-// Add global variables here
-
+const URL_TYPE19STELLAR = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/19.png";
+const URL_TYPE10001UNKNOWN = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-iv/platinum/10001.png";
+const URL_ALLPOKEMON = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100000";
+const URL_POKEMONVIAID = "https://pokeapi.co/api/v2/pokemon/";
+const URL_TYPEIMG = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/brilliant-diamond-and-shining-pearl/";
+const ERROR_FETCHCATCH = "Schnittstellen-Aufruf ist fehlgeschlagen. Bitte versuchen sie es Später wieder.";
+const LIGHTOPACITY = "0.1";
+const DARKOPACITY = "0.9";
+const LOADAMOUNT = 30;
+const TYPES = [
+    {
+        "typeName": "normal",
+        "id" : 1,
+        "lightColorCode" : `rgba(153,153,153,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(153,153,153,${DARKOPACITY})`
+    },
+    {
+        "typeName": "fighting",
+        "id" : 2,
+        "lightColorCode" : `rgba(255,162,2,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(255,162,2,${DARKOPACITY})`
+    },
+    {
+        "typeName": "flying",
+        "id" : 3,
+        "lightColorCode" : `rgba(149,201,255${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(149,201,255${DARKOPACITY})`
+    },
+    {
+        "typeName": "poison",
+        "id" : 4,
+        "lightColorCode" : `rgba(153,77,207,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(153,77,207,${DARKOPACITY})`
+    },
+    {
+        "typeName": "ground",
+        "id" : 5,
+        "lightColorCode" : `rgba(171,121,57,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(171,121,57,${DARKOPACITY})`
+    },
+    {
+        "typeName": "rock",
+        "id" : 6,
+        "lightColorCode" : `rgba(188,184,137,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(188,184,137,${DARKOPACITY})`
+    },
+    {
+        "typeName": "bug",
+        "id" : 7,
+        "lightColorCode" : `rgba(159,164,36,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(159,164,36,${DARKOPACITY})`
+    },
+    {
+        "typeName": "ghost",
+        "id" : 8,
+        "lightColorCode" : `rgba(113,71,113,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(113,71,113,${DARKOPACITY})`
+    },
+    {
+        "typeName": "steel",
+        "id" : 9,
+        "lightColorCode" : `rgba(106,174,211,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(106,174,211,${DARKOPACITY})`
+    },
+    {
+        "typeName": "fire",
+        "id" : 10,
+        "lightColorCode" : `rgba(255,97,44,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(255,97,44,${DARKOPACITY})`
+    },
+    {
+        "typeName": "water",
+        "id" : 11,
+        "lightColorCode" : `rgba(41,146,255,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(41,146,255,${DARKOPACITY})`
+    },
+    {
+        "typeName": "grass",
+        "id" : 12,
+        "lightColorCode" : `rgba(66,191,36,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(66,191,36,${DARKOPACITY})`
+    },
+    {
+        "typeName": "electric",
+        "id" : 13,
+        "lightColorCode" : `rgba(255,219,0,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(255,219,0,${DARKOPACITY})`
+    },
+    {
+        "typeName": "psychic",
+        "id" : 14,
+        "lightColorCode" : `rgba(255,99,127,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(255,99,127,${DARKOPACITY})`
+    },
+    {
+        "typeName": "ice",
+        "id" : 15,
+        "lightColorCode" : `rgba(66,191,255,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(66,191,255,${DARKOPACITY})`
+    },
+    {
+        "typeName": "dragon",
+        "id" : 16,
+        "lightColorCode" : `rgba(84,98,214,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(84,98,214,${DARKOPACITY})`
+    },
+    {
+        "typeName": "dark",
+        "id" : 17,
+        "lightColorCode" : `rgba(79,71,71,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(79,71,71,${DARKOPACITY})`
+    },
+    {
+        "typeName": "fairy",
+        "id" : 18,
+        "lightColorCode" : `rgba(255,177,255,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(255,177,255,${DARKOPACITY})`
+    },
+    {
+        "typeName": "stellar",
+        "id" : 19,
+        "lightColorCode" : `rgba(0,0,0,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(0,0,0,${DARKOPACITY})`
+    },
+    {
+        "typeName": "unknown",
+        "id" : 10001,
+        "lightColorCode" : `rgba(0,0,0,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(0,0,0,${DARKOPACITY})`
+    },
+    {
+        "typeName": "myUnknown",
+        "id" : 99999,
+        "lightColorCode" : `rgba(0,0,0,${LIGHTOPACITY})`,
+        "darkColorCode" : `rgba(0,0,0,${DARKOPACITY})`
+    }
+];
+let allPokemons =[]; // {name, url}
+let loadetPokemons = []; // detailinfos
+let currentlyRendertCounter = 0;
 
 /*====================================================================================================
     FUNCTIONS
 ====================================================================================================*/ 
-// add functions here
-/*
+/**
 * Description for the function
 * @param {string} <variableName> Desription for the usage of a parameter
 * @param {number} <variableName> Desription for the usage of a parameter
@@ -16,9 +153,100 @@
 * @param {(number|Array)} <variableName> Desription for the usage of a parameter
 * @returns {(string|Array)} <variableName> Desription for the return variable/value
 */
+async function init(){
+    await setAllPokemons();
+    renderCards_Amount(currentlyRendertCounter,LOADAMOUNT);
+}
+
+/**
+* Description for the function
+* @param {string} <variableName> Desription for the usage of a parameter
+* @param {number} <variableName> Desription for the usage of a parameter
+* @param {(string|Array)} <variableName> Desription for the usage of a parameter
+* @param {(number|Array)} <variableName> Desription for the usage of a parameter
+* @returns {(string|Array)} <variableName> Desription for the return variable/value
+*/
+async function renderCards_Amount(start, amount){
+    console.log(amount);
+    console.log(allPokemons);
+    console.log(currentlyRendertCounter);
+    
+    await setPokemonDetails(start, amount);
+    let cardsContainerRef = document.getElementById('cardsContainer');
+    cardsContainerRef.innerHTML="";
+    for (let i = start; i < (start+amount); i++){
+        if(!(start+amount > allPokemons.length)){
+            currentlyRendertCounter++;
+            let arrayOfTypeIds = getTypeIds(loadetPokemons[i]);
+            if (arrayOfTypeIds.length>1){
+                cardsContainerRef.innerHTML += getHTMLForCardWithTwoTypes(loadetPokemons[i], i, URL_TYPEIMG + arrayOfTypeIds[0] + ".png", URL_TYPEIMG + arrayOfTypeIds[1] + ".png");
+            }else{
+                cardsContainerRef.innerHTML += getHTMLForCardWithOneType(loadetPokemons[i], i, URL_TYPEIMG + arrayOfTypeIds[0] + ".png");
+            }
+        }
+    }
+}
+
+function renderCards_Ids(array){
+    console.log(amount);
+    console.log(allPokemons);
+    console.log(currentlyRendertCounter);
+}
+
+/**
+* Description for the function
+* @param {string} <variableName> Desription for the usage of a parameter
+* @param {number} <variableName> Desription for the usage of a parameter
+* @param {(string|Array)} <variableName> Desription for the usage of a parameter
+* @param {(number|Array)} <variableName> Desription for the usage of a parameter
+* @returns {(string|Array)} <variableName> Desription for the return variable/value
+*/
+async function setAllPokemons(){
+    try {
+        let listOfAllPokemon = await fetch(URL_ALLPOKEMON);
+        let allPokemonsResponse = await listOfAllPokemon.json();
+        allPokemons = allPokemonsResponse.results;
+    } catch (error) {
+        console.error(ERROR_FETCHCATCH);
+    }
+}
+
+async function setPokemonDetails(start, amount){
+    for (let i = start; i < (start+amount); i++) {
+        try {
+            let detailsPokemonX = await fetch(URL_POKEMONVIAID+(i+1));
+            loadetPokemons.push(await detailsPokemonX.json());
+        } catch (error) {
+            console.error(ERROR_FETCHCATCH); 
+        }
+    }
+}
+
+function getTypeIds(pokemon){
+    let foundTypes = [];
+    pokemon.types.forEach((type, i) => {
+        foundTypes.push(pokemon.types[i].type.name)    
+    });
+
+    let foundIds = [];
+    for (let i = 0; i < foundTypes.length; i++) {
+         TYPES.forEach(type => {
+            if(type.typeName == foundTypes[i]){
+                foundIds.push(type.id);
+            }
+         });
+    }
+    return foundIds;
+}
+
+function reset(){
+    currentlyRendertCounter=0;
+}
 
 /*====================================================================================================
     EVENT LISTENERS
 ====================================================================================================*/ 
 // Add eventlistenders here
+
+
 
---
+++ scripts/templateOverviewCard.js
@@ -0,0 +1,38 @@
+function getHTMLForCardWithOneType(loadetPokemon, index, typeURL) {
+    return `
+                    <div class="card" id="card${index}">
+                        <img src="${loadetPokemon.sprites.other.dream_world.front_default}"
+                            alt="" class="card__Img" id="card__Img${index}">
+                        <p class="card__No" id="card__No1">No #${index + 1}</p>
+                        <p class="card__Name" id="card__Name${index}">${loadetPokemon.name}</p>
+                        <div class="card__Types" id="card__Types${index}">
+                            <div class="card__type">
+                                <img src="${typeURL}"
+                                    alt="" class="type__Img" id="type__Img${index}1">
+                            </div>
+                        </div>
+                    </div>
+    `
+}
+
+function getHTMLForCardWithTwoTypes(loadetPokemon, index, type1URL, type2URL) {
+    return `
+                    <div class="card" id="card${index}">
+                        <img src="${loadetPokemon.sprites.other.dream_world.front_default}"
+                            alt="" class="card__Img" id="card__Img${index}">
+                        <p class="card__No" id="card__No1">No #${index + 1}</p>
+                        <p class="card__Name" id="card__Name${index}">${loadetPokemon.name}</p>
+                        <div class="card__Types" id="card__Types${index}">
+                            <div class="card__type">
+                                <img src="${type1URL}"
+                                    alt="" class="type__Img" id="type__Img${index}1">
+                            </div>
+                            <div class="card__type">
+                                <img src="${type2URL}"
+                                    alt="" class="type__Img" id="type__Img${index}2">
+                            </div>
+                        </div>
+                    </div>
+    `
+}
+
